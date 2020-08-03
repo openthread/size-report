fd3955624e3caabadbd5f571c3313045b7718344
chris98122 <chris98122@126.com>
2020-08-03 17:13:15 +0800

    parent_commit_id

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ compare_url = sys.argv[2]
 repo_name = sys.argv[3]
 #openthread
 
-last_commit_id = ""
+newest_commit_id = ""
+parent_commit_id = ""
 
 retcode, output = subprocess.getstatusoutput("cd .. \n" + "git clone " + clone_url)
 print("git clone retcode is: %s" % retcode);#0表示执行成功,否则表示失败
@@ -23,9 +24,13 @@ if retcode != 0:
     if retcode != 0:
         sys.exit("git pull failed") 
     else:
-        last_commit_id, output = subprocess.getstatusoutput("git rev-parse HEAD") # git 获取最近一次提交的commit id
+        retcode, newest_commit_id = subprocess.getstatusoutput("git rev-parse HEAD") # git 获取最近一次提交的commit id
         print(output)
-        print("last_commit_id is: %s" % last_commit_id)
+        print("newest_commit_id is: %s" % newest_commit_id)
+
+        retcode, parent_commit_id = subprocess.getstatusoutput("git rev-parse HEAD^")# git 获取parent commit id
+        print(output)
+        print("parent_commit_id is: %s" % parent_commit_id)
 
 # SIZE_REPORT_URL =http://localhost:3000/  
 # compile : make -f examples/Makefile-nrf52840 "
@@ -34,26 +39,25 @@ if retcode != 0:
 retcode, output = subprocess.getstatusoutput("cd ../" + repo_name + "\n" + " ./script/check-size nrf52840")
 print(output)
 if retcode != 0:
-    sys.exit("check-size failed")
-
+    print("check-size failed")
 
 with open('/tmp/size_report') as f:
     lines = f.readlines()
     filename = ""
     for line in lines:
         splitted = line.split('|')
-        if (lines.index(line) - 2 ) % 3 == 0:
-            filename = splitted[1]
-        # if (lines.index(line) - 3 ) % 3 == 0:
-        commit_id = splitted[2]
         text = splitted[3]
         data = splitted[4]
         bss = splitted[5]
         total = splitted[6]
 
-        filechange = {"commit_id":commit_id,"code_size":{filename:{"text":text , "data" :data ,"bss":bss, "total":total }}}
-        print(file_change) 
+        if (lines.index(line) - 2 ) % 3 == 0:
+            filename = splitted[1]
+            parent_filechange = {"commit_id":parent_commit_id,"code_size":{filename:{"text":text , "data" :data ,"bss":bss, "total":total }}}
+            print(parent_filechange) 
 
-
-
+        if (lines.index(line) - 3 ) % 3 == 0:
+            filechange = {"commit_id":newest_commit_id,"code_size":{filename:{"text":text , "data" :data ,"bss":bss, "total":total }}}
+            print(filechange) 
+        
 #test cmd : python3 main.py https://github.com/chris98122/openthread.git 1 openthread
