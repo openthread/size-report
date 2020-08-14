1a3950e91234094590edcb791e3dae6d3ba45f54
Murphy-OrangeMud <chengzhiyi_2@163.com>
2020-08-14 15:12:02 +0800

    Add API and fix exception handling

--- backend/data.py
+++ backend/data.py
@@ -26,9 +26,12 @@ def retrieve_commit_data():
 
         commit = json["commits"][0]
         parent_commit = json["commits"][1]
+        
+        print(commit["commit_id"])
 
-        if db.execute("select * from commitinfo where id = ?", (commit["commit_id"],)).fetchall() != None:
-            return jsonify({"status": "OK"})
+        if len(db.execute("select * from commitinfo where id = ?", (commit["commit_id"],)).fetchall()) > 0:
+            print(db.execute("select * from commitinfo where id = ?", (commit["commit_id"],)).fetchall())
+            return jsonify({"status": "Exist commit"})
 
         db.execute("insert into commitinfo values('{}','{}','{}');".format(commit["commit_id"], commit["timestamp"], parent_commit["parent_commit_id"]))
         db.commit()
@@ -65,11 +68,16 @@ def retrieve_commit_data():
 def latest_commit_id():
     db = get_db()
 
-    return jsonify(db.execute("select id from commitinfo order by created_at desc").fetchone()[0])
+    commit = db.execute("select id from commitinfo order by created_at desc").fetchone()
+
+    if commit == None:
+        return jsonify({"status": "No commit in the database"})
+
+    return jsonify(commit[0])
 
 
-@app.route("/ncommitinfo", methods=("POST",))
-def get_n_commits():
+@app.route("/nlatestcommitinfo", methods=("POST",))
+def get_n_latest_commits():
     db = get_db()
 
     if request.method == "POST":
@@ -141,11 +149,13 @@ def get_commit_info():
         json = request.get_json()
         
         commit_id = json["commit_id"]
-        print(commit_id)
 
         basic_info = db.execute("select * from "
                                 "commitinfo where id = ?",
                                 (commit_id,),).fetchone()
+
+        if basic_info == None:
+            return jsonify({"status": "Inexist commit"})
 
         fileinfo = db.execute("select * from "
                             "filechange where commit_id = ?",
@@ -160,9 +170,80 @@ def get_commit_info():
                                         "total": item["totalsize"]}
 
         return jsonify(
+            status="OK",
             commit_id=basic_info["id"],
             parent_id=basic_info["parent_id"],
             created_time=basic_info["created_at"],
             code=files
         )
+
+
+@app.route("/ncommitinfo", methods=("POST",))
+def get_n_commits():
+    db = get_db()
+
+    if request.method == "POST":
+        content_type = request.headers["Content-type"]
+
+        if content_type != "application/json":
+            return jsonify({"status": "Incorrect content_type"})
+
+        json = request.get_json()
+        
+        n = json["n_commit_info"]
+        commit_id = json["latest_commit_id"]
+
+        specified_commit = db.execute("select * from commitinfo where id = ?", (commit_id,)).fetchone()
+
+        if specified_commit == None:
+            return jsonify({"status": "Incorrect content_type"})
+
+        specified_timestamp = specified_commit["created_at"]
+
+        if int(db.execute("select count(*) from commitinfo where created_at <= ?", (specified_timestamp, )).fetchall()[0][0]) < int(n):
+            basic_info = db.execute("select * from commitinfo where created_at <= ? order by created_at desc", (specified_timestamp,)).fetchall()
+
+            return_value = []
+            
+            for it in basic_info:
+                commitid = it[0]
+                filesinfo = db.execute("select * from filechange where commit_id = ?", (commitid,),).fetchall()
+
+                files = dict()
+                
+                for item in filesinfo:
+                    files[item["file_name"]] = {"text": item["textsize"], 
+                                                "data": item["datasize"], 
+                                                "bss": item["bsssize"], 
+                                                "total": item["totalsize"]}
+
+                commit_info = {"commit_id": commitid, "parent_id": it[2], "created_time": it[1], "code": files}
+                return_value.append(commit_info)
+
+            return jsonify(return_value)
+
+        else:
+            basic_info = db.execute("select * from commitinfo where created_at <= ? order by create_at desc").fetchall()[:n]
+
+            return_value = []
+            
+            for it in basic_info:
+                commitid = it[0]
+                filesinfo = db.execute("select * from filechange where commit_id = ?", (commitid,),).fetchall()
+
+                files = dict()
+                
+                for item in filesinfo:
+                    files[item["file_name"]] = {"text": item["textsize"], 
+                                                "data": item["datasize"], 
+                                                "bss": item["bsssize"], 
+                                                "total": item["totalsize"]}
+
+                commit_info = {"commit_id": commitid, "parent_id": it[2], "created_time": it[1], "code": files}
+                return_value.append(commit_info)
+
+            return jsonify(
+                status="OK",
+                commits=return_value
+            )
 
